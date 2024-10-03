e4fb4c0abf04ac877795aeb263d45b1dab11dff9
Writwick Das <writzx@gmail.com>
2024-10-03 12:23:03 +0200

    cmake config

--- src/global.h
+++ src/global.h
@@ -2,4 +2,10 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
+#ifdef FINDER_EXPORTS
+    #define FINDER_API __declspec(dllexport)
+#else
+    #define FINDER_API __declspec(dllimport)
+#endif
+
 extern HMODULE ADDON_MODULE;
